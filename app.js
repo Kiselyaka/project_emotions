8c66acb59c39d0553c6701aeceb8f7e8f8c2bad6
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-26 00:14:29 +0300

    Пункт 8_2

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost/emotions2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var emotions = require('./routes/emotions');
 const { title } = require('process');
 
 var app = express();
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/emotions', emotions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
