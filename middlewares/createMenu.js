8786830be637823b15a31b84412850b3e8354010
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-26 17:12:51 +0300

    Пункт 9_5

---
+++ middlewares/createMenu.js
@@ -0,0 +1,14 @@
+var Emotiion = require("../models/emotion").Emotion
+
+module.exports = async function(req,res,next)
+{
+    res.locals.nav = []
+
+    var menu = await Emotiion.find(null,{_id:0,title:1,nick:1});
+    console.log(menu);
+    if (menu.length != 0) 
+    {
+        res.locals.nav = menu;
+    }
+    next();
+}
