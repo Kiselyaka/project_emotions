ac543a8214fd032aa8af8ded208c19bddfa2b360
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-07 19:03:48 +0300

    Пункт 10_3

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,11 @@ router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
 });
 
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  console.log(username);
+  console.log(password);
+  });
   
 module.exports = router;
